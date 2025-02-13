dfea8fe330a620f9d54c4fc459f6219a30fa9898
Dorian Kita <dorian.kita90@gmail.com>
2025-02-13 20:56:00 +0100

    Created complete ui view

--- ui.py
+++ ui.py
@@ -5,8 +5,19 @@ THEME_COLOR = "#375362"
 class QuizInterface():
 
     def __init__(self):
+        self.score = 0
         self.window = Tk()
         self.window.title("Quizzler")
+        self.window.config(pady=20, padx=20)
+
+        self.label = Label(text=f"Score: {self.score}")
+        self.label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, background="white")
+        self.text = self.canvas.create_text(150,125, text="Example text to be replaced by questions coming from our "
+                                                          "API based question data", fill="black", width=280,
+                                            font=("Arial", 20, 'italic'))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         self.true_button_image = PhotoImage(file='images/true.png')
         self.true = Button(image=self.true_button_image)
