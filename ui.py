38d2b38e491d0f9cb052ff5b6ef7e6976ea9ba8a
Dorian Kita <dorian.kita90@gmail.com>
2025-02-13 20:19:52 +0100

    Created QuizInterface with buttons

--- ui.py
+++ ui.py
@@ -1,2 +1,20 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface():
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+
+        self.true_button_image = PhotoImage(file='images/true.png')
+        self.true = Button(image=self.true_button_image)
+        self.true.grid(row= 2, column = 0)
+
+        self.false_button_image = PhotoImage(file='images/false.png')
+        self.false = Button(image=self.false_button_image)
+        self.false.grid(row=2, column=1)
+
+
+        self.window.mainloop()
